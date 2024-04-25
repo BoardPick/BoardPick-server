35cbde3ce7133a125cc4735d8579b922e30df258
AhnJoonSung <tjdtna01@naver.com>
2024-04-25 17:23:38 +0900

    first commit

---
+++ src/main/java/com/swyp/boardpick/BoardpickApplication.java
@@ -0,0 +1,13 @@
+package com.swyp.boardpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardpickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardpickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swyp/boardpick/BoardpickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swyp.boardpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardpickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
