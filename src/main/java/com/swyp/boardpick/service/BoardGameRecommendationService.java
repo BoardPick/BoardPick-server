67a435497d03bb91d9e250a6107e0ff8ad87ffe0
AhnJoonSung <tjdtna01@naver.com>
2024-05-18 02:39:03 +0900

    Fix: 보드게임 상세조회, 추천 보드게임 조회 API에서 Dto 응답으로 수정

--- src/main/java/com/swyp/boardpick/service/BoardGameRecommendationService.java
+++
@@ -1,9 +0,0 @@
-package com.swyp.boardpick.service;
-
-import com.swyp.boardpick.domain.BoardGame;
-
-import java.util.List;
-
-public interface BoardGameRecommendationService {
-    List<BoardGame> recommendBoardGames();
-}
