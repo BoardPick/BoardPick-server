fd03ec53a0f07fdfd96bde65b786a78e0e234490
AhnJoonSung <tjdtna01@naver.com>
2024-05-19 12:44:28 +0900

    checkout을 위한 임시 커밋temp

--- src/main/java/com/swyp/boardpick/domain/Uri.java
+++ src/main/java/com/swyp/boardpick/domain/Uri.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum Uri {
-    HTTP_FOUND("https://boardpick.netlify.app/onBoarding");
+    HTTP_FOUND("https://boardpick.netlify.app/onBoarding"),
+    MAIN_PAGE("https://boardpick.netlify.app"),
+    LOGOUT_REDIR("http://localhost:8080/logout");
 
     private final String description;
 }
