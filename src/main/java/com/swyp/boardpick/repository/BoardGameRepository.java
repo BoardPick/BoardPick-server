a9d939d30d5e345544905c0edaec10437d1a0ed5
tmddus2 <tmddus2__@naver.com>
2024-05-22 10:45:14 +0900

    fix: 난이도 상위 10개 보드게임 조회 쿼리문 수정

--- src/main/java/com/swyp/boardpick/repository/BoardGameRepository.java
+++ src/main/java/com/swyp/boardpick/repository/BoardGameRepository.java
@@ -21,10 +21,10 @@ public interface BoardGameRepository extends JpaRepository<BoardGame, Long> {
             "ORDER BY COUNT(ubg) DESC")
     Page<BoardGame> findByPickCountDescForToday(Pageable pageable);
 
-    @Query("SELECT bg FROM BoardGame bg ORDER BY COUNT(bg.difficulty) DESC")
+    @Query("SELECT bg FROM BoardGame bg ORDER BY bg.difficulty DESC")
     Page<BoardGame> findByPickDifficultyDesc(Pageable pageable);
 
-    @Query("SELECT bg FROM BoardGame bg WHERE bg.maxPlayers=2 OR bg.minPlayers=2")
+    @Query("SELECT bg FROM BoardGame bg WHERE bg.maxPlayers=2 AND bg.minPlayers=2")
     Page<BoardGame> findByPick2PlayersDesc(Pageable pageable);
 
     @Query("SELECT bg FROM BoardGame bg ORDER BY COUNT(bg.maxPlayers) DESC")
--- src/main/java/com/swyp/boardpick/service/implement/BoardGameService.java
+++ src/main/java/com/swyp/boardpick/service/implement/BoardGameService.java
@@ -4,9 +4,12 @@ import com.swyp.boardpick.domain.*;
 import com.swyp.boardpick.dto.response.BoardGameDto;
 import com.swyp.boardpick.repository.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -109,29 +112,12 @@ public class BoardGameService {
     }
 
     public List<BoardGameDto> getTop10(String filter) {
+        Page<BoardGame> boardGames = boardGameRepository.findByPick2PlayersDesc(PageRequest.of(0,10));
         if (filter.equals("difficulty")) {
-            return boardGameRepository.findByPickDifficultyDesc(PageRequest.of(0,10))
-                    .map(boardGame -> {
-                        List<String> tags = boardGame.getBoardGameTags()
-                                .stream().map(boardGameTag -> boardGameTag.getTag().getContent())
-                                .toList();
-                        return new BoardGameDto(boardGame, tags);
-                    }).toList();
+            boardGames = boardGameRepository.findByPickDifficultyDesc(PageRequest.of(0,10));
         } else if (filter.equals("players")) {
-            return boardGameRepository.findByPickPlayersDesc(PageRequest.of(0,10))
-                    .map(boardGame -> {
-                        List<String> tags = boardGame.getBoardGameTags()
-                                .stream().map(boardGameTag -> boardGameTag.getTag().getContent())
-                                .toList();
-                        return new BoardGameDto(boardGame, tags);
-                    }).toList();
+            boardGames = boardGameRepository.findByPickPlayersDesc(PageRequest.of(0,10));
         }
-        return boardGameRepository.findByPick2PlayersDesc(PageRequest.of(0,10))
-                .map(boardGame -> {
-                    List<String> tags = boardGame.getBoardGameTags()
-                            .stream().map(boardGameTag -> boardGameTag.getTag().getContent())
-                            .toList();
-                    return new BoardGameDto(boardGame, tags);
-                }).toList();
+        return boardGames.map(boardGame -> convertToDto(boardGame)).stream().toList();
     }
 }
